4386d6e473b078513c0cf8e9356f279ad4bc61f5
Harrison Strand <harrisonstrand@STRAND.local>
2023-07-31 19:26:29 -0700

    users controller - read and update functions for users and user friends

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
 import { register } from './controllers/auth.js';
 
 // CONFIGURATIONS
@@ -27,7 +28,6 @@ app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // FILE STORAGE
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/assets");
@@ -40,12 +40,12 @@ const upload = multer({ storage });
 
 //ROUTES WITH FILES
 app.post('/auth/register', upload.single('picture'), register);
+app.use('/users', userRoutes)
 
 //ROUTES
 app.use('/auth', authRoutes);
 
 // MONGOOSE SETUP
-
 const PORT = process.env.PORT || 6001;
 mongoose
   .connect(process.env.MONGO_URL, {
