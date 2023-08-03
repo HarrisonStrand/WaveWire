0fd3da3d3c7bbfd1eb73625a513348f12fc3624d
Harrison Strand <harrisonstrand@STRAND.local>
2023-08-02 21:21:28 -0700

    add dark and light theme color data

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
 import { createPost } from './controllers/posts.js';
 import { verifyToken }  from './middleware/auth.js';
+import User from './models/User.js';
+import Post from "./models/Post.js";
+import { users, posts } from './data/index.js';
+
 
 // CONFIGURATIONS
 
@@ -59,5 +63,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    //Manually injecting - ADD DATA ONE TIME ONLY
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
